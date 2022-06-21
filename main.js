4f5207597f555ffede1c2559314d5c95560f36b9
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-06-21 18:44:38 +0200

    Adjust css

--- main.js
+++ main.js
@@ -10,13 +10,3 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
-
-  //preloader
-  window.addEventListener("load",()=>{
-
-    document.querySelector(".preloader").classList.add("fade-out");
-    setTimeout(() =>{
-  document.querySelector(".preloader").style.display="none"
-    },600)
-  })
-  
