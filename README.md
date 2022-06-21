3f0657cc15db276a239dd75c69640a3dd855c5d8
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-06-21 17:57:45 +0200

    Increase performance
    Adapt image to the web

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Capstone-1
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Microverse-Module-1
+## Capstone Project
+
+> In this capstone project, I build a website for a charity-based event to learn and practice using semantic HTML tags, apply best practices in HTML code use CSS selectors correctly use the CSS box model use Flexbox to place elements on the page to demonstrate the ability to create UIs adaptable to different screen sizes using media queries to use GitHub Pages to deploy web pages to apply JavaScript best practices and language style guides in code to use JavaScript to manipulate DOM elements to use JavaScript events to use objects to store and access data and to communicate technical concepts to other technical people.
+
+
+
+## Built With
+
+- HTML , CSS & JS
+
+## Live Demo
+
+[Checkout the Live Demo](https://chrispaixk.github.io/Capstone-1/)
+
+## Getting Started
+
+**Create a file locally and clone this project**
+
+To get a local copy up and running follow these simple example steps.
+
+### create a local folder
+
+### open your code editor
+
+### open your folder in your editor
+
+### clone the project from Github
+
+[Here](https://github.com/ChrispaixK/Capstone-1/tree/lab)
+
+
+## Authors
+
+👤 **Chrispaix KAZE**
+
+- GitHub: [@ChrispaixK](https://github.com/ChrispaixK)
+- Twitter: [@ChrispaixK](https://twitter.com/ChrispaixK)
+- LinkedIn: [Chrispaix KAZE](https://www.linkedin.com/in/chrispaix-kaze-70445a175/)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- MicroverseInc
+- [Pexels](https://www.pexels.com/)
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- home.js
+++ home.js
@@ -2,42 +2,36 @@
 
 const items = [
   {
-    backImage: 'img/checkerboard-squares-black-white.jpg',
     profilePic: 'img/1.png',
     name: 'Maria Hussain',
     jobDescr: 'Full Stack remote Software Developer at Microsoft California',
     comment: 'Person more about that person more about that person more about that person',
   },
   {
-    backImage: 'img/checkerboard-squares-black-white.jpg',
     profilePic: 'img/2.png',
     name: 'Tarek Ali',
     jobDescr: 'Full Stack remote Software Developer at Microsoft California',
     comment: 'Person more about that person more about that person more about that person',
   },
   {
-    backImage: 'img/checkerboard-squares-black-white.jpg',
     profilePic: 'img/3.png',
     name: 'Abdelmejid Mohamad',
     jobDescr: 'Full Stack remote Software Developer at Microsoft California',
     comment: 'Person more about that person more about that person more about that person',
   },
   {
-    backImage: 'img/checkerboard-squares-black-white.jpg',
     profilePic: 'img/4.png',
     name: 'Abraham Ibizugbe',
     jobDescr: 'Full Stack remote Software Developer at Microsoft California',
     comment: 'Person more about that person more about that person more about that person',
   },
   {
-    backImage: 'img/checkerboard-squares-black-white.jpg',
     profilePic: 'img/5.png',
     name: 'Emma techie',
     jobDescr: 'Full Stack remote Software Developer at Microsoft California',
     comment: 'Person more about that person more about that person more about that person',
   },
   {
-    backImage: 'img/checkerboard-squares-black-white.jpg',
     profilePic: 'img/6.png',
     name: 'Piesye Piesye',
     jobDescr: 'Full Stack remote Software Developer at Microsoft California',
@@ -51,7 +45,7 @@ for (let i = 0; i < items.length; i += 1) {
   data.innerHTML = `
   <div class="staff-item">
   <div class="staff-img">
-        <img src="${items[i].backImage}" alt="back" class="back"/> 
+        <img src="img/checkerboard-squares-black-white.svg" alt="back" class="back"/> 
         <img src="${items[i].profilePic}" alt="staff" class="profile"/> 
       </div>
       <div class="staff-info">
