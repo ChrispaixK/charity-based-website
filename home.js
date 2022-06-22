1247cb6764f15b15b810a814b750278390c0cbd6
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-06-22 11:06:59 +0200

    Style home and about pages
    Add shadows on about and home page , add scroll up button

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-/* speak section dynmically */
+/* staff section dynmically */
 
 const items = [
   {
--- main.js
+++ main.js
@@ -10,3 +10,22 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+// scrollUp
+const scrollUp = document.getElementById('myBtn');
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUp.style.display = 'block';
+  } else {
+    scrollUp.style.display = 'none';
+  }
+}
+
+window.onscroll = function () { scrollFunction(); };
+
+const goUp = document.querySelector('.scrollUp');
+goUp.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
